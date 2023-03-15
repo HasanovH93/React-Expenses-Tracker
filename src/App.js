5b3d73565c3a8834caa165fc530b44ad37cd2362
HasanovH93 <hasanovh14@gmail.com>
2023-03-15 20:22:35 +0100

    display expenses

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 290.99,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450.99,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
