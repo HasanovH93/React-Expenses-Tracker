f9966e1f011a00014dede57f6ef740730c2e6cc5
HasanovH93 <hasanovh14@gmail.com>
2023-03-16 09:35:52 +0100

    set expenses chart

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./NewExpense.css";
 import NewExpenseForm from "./NewExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   let currentId = 5;
   function generateId() {
     const id = "e" + currentId;
@@ -16,9 +19,25 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
   return (
     <div className="new-expense">
-      <NewExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <NewExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
